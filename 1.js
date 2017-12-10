300ce3c34298989f0f9d1d663d4ea8760b7a10a2
ben <ben@123.com>
2017-12-10 14:24:18 +0800

    Revert "Revert "cm""
    This reverts commit 1de0e72733f2721ccd781de375600e88502ef6ff.

---
+++ 1.js
@@ -0,0 +1,8 @@
+var b = require('./com.js')
+var a = require('./2.js')
+console.log('--------------')
+console.log('111111111111111')
+console.log(b)
+console.log(a.a)
+console.log(a.a == b)
+console.log('--------------')
---
+++ com.js
@@ -0,0 +1,2 @@
+console.log('here is com')
+module.exports = { a: 123123 }
